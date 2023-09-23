ef4aff69bd31a36b576df48236f3d3102639700f
jorozcoesparza <jorozco842@gmail.com>
2023-09-23 09:24:19 -0500

    Defines all basic arithemtic functions

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function addition(a, b) {
     return a + b; 
 
 }
+
+function subtraction(a, b) {
+
+    return a - b; 
+
+}
+
+function multiply(a, b) {
+
+    return a * b; 
+
+}
+
+function divide(a, b) {
+
+    return a / b; 
+
+}
