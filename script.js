2274a5faf5c9bade2aba52ac4636f90050711aed
jorozcoesparza <jorozco842@gmail.com>
2023-09-24 11:34:39 -0500

    Creates opreates function that calls other functions to execute calculation

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-let a; 
-let b;
+let varOne = 0; 
+let varTwo = 0;
+let operator = "";
 
-function addition(a, b) {
+function addition(varOne, varTwo) {
 
-    return a + b; 
-
-}
-
-function subtraction(a, b) {
-
-    return a - b; 
+    return varOne + varTwo; 
 
 }
 
-function multiply(a, b) {
+function subtraction(varOne, varTwo) {
 
-    return a * b; 
+    return varOne - varTwo; 
 
 }
 
-function divide(a, b) {
+function multiply(varOne, varTwo) {
 
-    return a / b; 
+    return varOne * varTwo; 
 
+}
+
+function divide(varOne, varTwo) {
+
+    return varOne / varTwo; 
+
+}
+
+function operate(operator, varOne, varTwo) {
+    if (operator == "+") {
+        return addition(varOne, varTwo)
+    } else if (operator == "-") {
+        return subtraction(varOne, varTwo)
+    } else if (operator == "*") {
+        return multiply(varOne, varTwo)
+    } else {
+        return divide(varOne, varTwo)
+    }
 }
